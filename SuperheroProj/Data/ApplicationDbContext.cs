acf578135f615092d8a14ced2287d9752619d12f
Brad Toney <bravotango87@gmail.com>
2020-05-19 12:01:03 -0400

    update the database to show table created.

--- SuperheroProj/Data/ApplicationDbContext.cs
+++ SuperheroProj/Data/ApplicationDbContext.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SuperheroProj.Models;
 
 namespace SuperheroProj.Data
 {
+
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Superhero> Superheroes { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-            DbSet<Hero>Heroes
+
         }
     }
 }
